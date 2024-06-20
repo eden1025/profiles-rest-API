2e12c0b0755aa580d547636d7a3ae74c49a2ab31
Eden Smith <eden.smith@udemy.com>
2024-06-20 10:07:52 -0700

    Added search/filtering of profiles

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -14,14 +14,14 @@ class UserProfileSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {
                 'write_only': True,
-                'style':{'input_type':'password'}
+                'style': {'input_type': 'password'}
             }
         }
     def create(self, validated_data):
         """Create and return a new user"""
         user = models.UserProfile.objects.create_user(
-            email= validated_data['email'],
-            name= validated_data['name'],
+            email=validated_data['email'],
+            name=validated_data['name'],
             password=validated_data['password']
         )
 
